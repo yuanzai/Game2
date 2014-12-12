128ec3656bdb07374aa448f4e0a021776ab04ba4
yuanzai <junyuan.lau@gmail.com>
2014-12-12 16:09:13 +0800

    2d array

---
+++ Game2/C2DArray_double.h
@@ -0,0 +1,12 @@
+#import <Cocoa/Cocoa.h>
+@interface C2DArray_double : NSObject 
+
+- (id)initWithRows:(NSUInteger)rowCount Columns:(NSUInteger)columnCount;
+
+- (NSUInteger)rowCount;
+- (NSUInteger)columnCount;
+
+- (double)valueAtRow:(NSUInteger)rowIndex Column:(NSUInteger)columnIndex;
+- (void)setValue:(double)value atRow:(NSUInteger)rowIndex Column:(NSUInteger)columnIndex;
+
+@end
