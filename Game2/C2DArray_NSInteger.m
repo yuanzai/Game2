0abce98e51673f8105f955f383f3f55251949f46
yuanzai <junyuan.lau@gmail.com>
2014-12-12 16:08:05 +0800

    Create 2darray

---
+++ Game2/C2DArray_NSInteger.m
@@ -0,0 +1,44 @@
+@implementation C2DArray_NSInteger{
+    NSInteger* matrix_;
+    NSUInteger columnCount_;
+    NSUInteger rowCount_;
+}
+
+- (id)initWithRows:(NSUInteger)initRowCount Columns:(NSUInteger)initColumnCount {
+    self = [super init];
+    if(self) {
+        rowCount_ = initRowCount;
+        columnCount_ = initColumnCount;
+        matrix_ = malloc(sizeof(int)*rowCount_*columnCount_);
+
+        uint i;
+        for(i = 0; i < rowCount_*columnCount_; ++i) {
+            matrix_[i] = 0;
+        }
+    }
+    return self;
+}
+
+- (void)dealloc {
+    free(matrix_);
+    [super dealloc];
+}
+
+- (NSUInteger)rowCount {
+    return rowCount_;
+}
+
+- (NSUInteger)columnCount {
+    return columnCount_;
+}
+
+- (NSInteger)valueAtRow:(NSUInteger)rowIndex Column:(NSUInteger)columnIndex 
+{
+    return matrix_[rowIndex*columnCount_+columnIndex];
+}
+- (void)setValue:(NSInteger)value atRow:(NSUInteger)rowIndex Column:(NSUInteger)columnIndex 
+{
+    matrix_[rowIndex*columnCount_+columnIndex] = value;
+}
+
+@end
